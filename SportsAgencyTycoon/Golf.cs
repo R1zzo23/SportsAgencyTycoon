a89b794c6f3adba3da5edf9499a84b95e8c076ba
Adam Rizzo <arizzo.dev@gmail.com>
2019-06-04 13:21:27 -0400

    continued working on golf tournament

--- SportsAgencyTycoon/Golf.cs
+++ SportsAgencyTycoon/Golf.cs
@@ -17,7 +17,7 @@ namespace SportsAgencyTycoon
             //lists to run tournament 
             List<Golfer> resultsList = new List<Golfer>();
             List<Golfer> tempList = new List<Golfer>();
-            List<Player> winnerList = new List<Player>();
+            List<Golfer> madeCutList = new List<Golfer>();
             List<Golfer> listOfGolfers = new List<Golfer>();
 
             //grab list of golfers from world.PGA
@@ -37,9 +37,6 @@ namespace SportsAgencyTycoon
             foreach (Golfer p in e.EntrantList) tempList.Add(p);
 
             /*
-             * 
-             * Code to run the actual tournament goes here!
-             *  - remaining players play two more rounds each
              *  - sort winners
              *  - if tied, play sudden death hole
              *  - AwardPayoutsAndTourPoints();
@@ -48,6 +45,9 @@ namespace SportsAgencyTycoon
              * 
              */
 
+            // every player in the tournament plays 2 rounds
+            // scores get added to List<int> CurrentTournamentScores 
+            // then added together for CurrentScore
             foreach (Golfer p in tempList)
             {
                 p.CurrentTournamentScores.Add(PlayRound(p, rnd));
@@ -62,44 +62,79 @@ namespace SportsAgencyTycoon
             foreach (Golfer p in tempList)
             {
                 if (p.CurrentScore > cutLine) resultsList.Add(p);
-                else winnerList.Add(p);
+                else madeCutList.Add(p);
             }
-            //scort resultsList from best to worst for standings sake
+            //sort resultsList from best to worst for standings sake
             resultsList = resultsList.OrderBy(o => o.CurrentScore).ToList();
 
             //remaining golfers play 2 more rounds
-            foreach (Golfer g in winnerList)
+            foreach (Golfer g in madeCutList)
             {
                 g.CurrentTournamentScores.Add(PlayRound(g, rnd));
                 g.CurrentTournamentScores.Add(PlayRound(g, rnd));
                 g.CurrentScore = CalculateCurrentTournamentScore(g);
             }
+
+            //sort madeCutList by score
+            madeCutList = madeCutList.OrderBy(o => o.CurrentScore).ToList();
+
+            //check for tie
+            if (IsThereATie(madeCutList))
+            {
+                List<Golfer> playoffList = new List<Golfer>();
+                //find the top score in the tournament
+                int bestScore = madeCutList[0].CurrentScore;
+                //add all golfers with matching score to playoffList
+                foreach (Golfer g in madeCutList)
+                {
+                    if (g.CurrentScore == bestScore) playoffList.Add(g);
+                }
+                //all golfers in playoffList play 1 hole at a time until someone wins
+                foreach (Golfer g in playoffList)
+                {
+                    g.CurrentScore += PlayHole(g, rnd);
+                }
+            }
+
             return results;
         }
+
         private int PlayRound(Golfer p, Random rnd)
         {
             int score = 0;
+
             for (int i = 0; i < 18; i++)
             {
-                int goodOrBadHole = rnd.Next(1, 101);
-                if (goodOrBadHole <= p.SkillLevel)
-                {
-                    //good hole
-                    int birdieOrBetter = rnd.Next(0, 50);
-                    if (birdieOrBetter >= 47) score -= 2; //eagle
-                    else if (birdieOrBetter >= 30) score--; //birdie
-                    else score += 0; //par
-                }
-                else
-                {
-                    //bad hole
-                    int meltDown = rnd.Next(0, 30);
-                    if (meltDown >= 23) score += 2; //double bogey
-                    else score++; //bogey
-                }
+                score += PlayHole(p, rnd);
             }
+
             return score;
         }
+
+        private int PlayHole(Golfer p, Random rnd)
+        {
+            int score = 0;
+            int goodOrBadHole = rnd.Next(1, 101);
+
+            if (goodOrBadHole <= p.SkillLevel)
+            {
+                //good hole
+                int birdieOrBetter = rnd.Next(0, 50);
+                if (birdieOrBetter >= 47) score -= 2; //eagle
+                else if (birdieOrBetter >= 30) score--; //birdie
+                else score += 0; //par
+            }
+            else
+            {
+                //bad hole
+                int meltDown = rnd.Next(0, 30);
+                if (meltDown >= 23) score += 2; //double bogey
+                else score++; //bogey
+            }
+
+            return score;
+        }
+
         private int CalculateCurrentTournamentScore(Golfer p)
         {
             int score = 0;
@@ -109,16 +144,26 @@ namespace SportsAgencyTycoon
             }
             return score;
         }
+
         private int DecideCutLine(List<Golfer> tempList)
         {
             tempList = tempList.OrderBy(o => o.CurrentScore).ToList();
             int medianIndex = tempList.Count / 2;
             return tempList[medianIndex].CurrentScore;
         }
+
+        private bool IsThereATie(List<Golfer> list)
+        {
+            int bestScore = list[0].CurrentScore;
+            if (list[1].CurrentScore == bestScore) return true;
+            else return false;
+        }
+
         private void AwardPayoutsAndTourPoints(Event e, List<Player> list, World world)
         {
 
         }
+
         public int CalculatePlayerTourPoints(Golfer g, World world)
         {
             int tourPoints = 0;
@@ -128,6 +173,7 @@ namespace SportsAgencyTycoon
 
             return tourPoints;
         }
+
         private void UpdatePGAPlayerList(List<Golfer> resultList, World world)
         {
             resultList = resultList.OrderByDescending(o => o.TourPoints).ToList();
