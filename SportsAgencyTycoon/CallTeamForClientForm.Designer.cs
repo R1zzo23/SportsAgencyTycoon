e63ae170de53ea7e84037b8d77c1bf94e2f36a8c
R1zzo23 <adamjrizzo@yahoo.com>
2019-07-20 21:00:42 -0400

    continued working on CallTeamForClientForm; player cards now generated

--- SportsAgencyTycoon/CallTeamForClientForm.Designer.cs
+++ SportsAgencyTycoon/CallTeamForClientForm.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.lblLeagueName = new System.Windows.Forms.Label();
             this.cbTeamList = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTitleContender = new System.Windows.Forms.Label();
+            this.lblMarketValue = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblLeagueName
@@ -49,16 +56,81 @@
             this.cbTeamList.Name = "cbTeamList";
             this.cbTeamList.Size = new System.Drawing.Size(222, 21);
             this.cbTeamList.TabIndex = 1;
+            this.cbTeamList.SelectedIndexChanged += new System.EventHandler(this.cbTeamList_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Age";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Skill";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contract";
+            // 
+            // lblTitleContender
+            // 
+            this.lblTitleContender.AutoSize = true;
+            this.lblTitleContender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleContender.Location = new System.Drawing.Point(264, 35);
+            this.lblTitleContender.Name = "lblTitleContender";
+            this.lblTitleContender.Size = new System.Drawing.Size(142, 20);
+            this.lblTitleContender.TabIndex = 3;
+            this.lblTitleContender.Text = "Title Contender: ";
+            // 
+            // lblMarketValue
+            // 
+            this.lblMarketValue.AutoSize = true;
+            this.lblMarketValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarketValue.Location = new System.Drawing.Point(505, 35);
+            this.lblMarketValue.Name = "lblMarketValue";
+            this.lblMarketValue.Size = new System.Drawing.Size(120, 20);
+            this.lblMarketValue.TabIndex = 4;
+            this.lblMarketValue.Text = "Market Value:";
             // 
             // CallTeamForClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMarketValue);
+            this.Controls.Add(this.lblTitleContender);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.cbTeamList);
             this.Controls.Add(this.lblLeagueName);
             this.Name = "CallTeamForClientForm";
             this.Text = "Let\'s Find You A Deal!";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,5 +140,11 @@
 
         private System.Windows.Forms.Label lblLeagueName;
         private System.Windows.Forms.ComboBox cbTeamList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitleContender;
+        private System.Windows.Forms.Label lblMarketValue;
     }
 }
--- SportsAgencyTycoon/CallTeamForClientForm.cs
+++ SportsAgencyTycoon/CallTeamForClientForm.cs
@@ -34,5 +34,50 @@ namespace SportsAgencyTycoon
                 cbTeamList.Items.Add(team);
             }
         }
+
+        public void CreatePlayerCards(Team team)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is GroupBox) this.Controls.Remove(control);
+            }
+
+            List<Player> positionPlayer = new List<Player>();
+            
+            foreach (Player p in team.Roster)
+                if (p.Position == _Client.Position)
+                {
+                    positionPlayer.Add(p);
+                }
+            for (int i = 0; i < positionPlayer.Count; i++)
+            {
+                GroupBox groupBox = new GroupBox();
+                groupBox.Location = new Point(12 + (i * 200 + 5), 65);
+                groupBox.Text = positionPlayer[i].FullName;
+                groupBox.Size = new Size(200, 100);
+                Label ageLabel = new Label();
+                ageLabel.Text = positionPlayer[i].Age.ToString() + "-years old";
+                ageLabel.Location = new Point(7, 20);
+                groupBox.Controls.Add(ageLabel);
+                Label skillLabel = new Label();
+                skillLabel.Text = "Skill Level: " + positionPlayer[i].CurrentSkill.ToString() + "/" + positionPlayer[i].PotentialSkill.ToString();
+                skillLabel.Location = new Point(7, 44);
+                groupBox.Controls.Add(skillLabel);
+                Label contractLabel = new Label();
+                contractLabel.Size = new Size(150, 13);
+                contractLabel.Text = "Years Left: " + positionPlayer[i].Contract.Years + " at " + positionPlayer[i].Contract.YearlySalary.ToString("C0") + " per season";
+                contractLabel.Location = new Point(7, 68);
+                groupBox.Controls.Add(contractLabel);
+                this.Controls.Add(groupBox);
+            }
+        }
+
+        private void cbTeamList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Team team = _League.TeamList[cbTeamList.SelectedIndex];
+            lblTitleContender.Text = "Title Contender: " + team.TitleConteder.ToString();
+            lblMarketValue.Text = "Market Value: " + team.MarketValue.ToString();
+            CreatePlayerCards(team);
+        }
     }
 }
