80739073d38c201bcf1d349a5c6826adf1bb2780
Adam Rizzo <arizzo.dev@gmail.com>
2019-08-22 16:37:03 -0400

    reworked simulating NBA games; need to code it for playoffs now

--- SportsAgencyTycoon/Basketball.cs
+++ SportsAgencyTycoon/Basketball.cs
@@ -43,6 +43,18 @@ namespace SportsAgencyTycoon
             int gamesThisWeek = HowManyGamesThisWeek();
             for (int i = 0; i < gamesThisWeek; i++)
             {
+                //int[] indexArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
+                var indexList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
+
+                for (int j = 0; j < NBA.TeamList.Count / 2; j++)
+                {
+                    int opponentIndex = rnd.Next(1, indexList.Count);
+                    Console.WriteLine("Team1 Index: " + NBA.TeamList[indexList[0]] + " - Team2 Index: " + NBA.TeamList[indexList[opponentIndex]]);
+                    SimulateGame(NBA.TeamList[indexList[0]], NBA.TeamList[indexList[opponentIndex]]);
+                    indexList.RemoveAt(opponentIndex);
+                    indexList.RemoveAt(0);
+                }
+                /*
                 foreach (Team t in NBA.TeamList) t.PlayedGameThisCycle = false;
                 for (int j = 0; j < NBA.TeamList.Count; j++)
                 {
@@ -65,6 +77,7 @@ namespace SportsAgencyTycoon
                         SimulateGame(NBA.TeamList[j], NBA.TeamList[opponentIndex]);
                     }
                 }
+                */
                 index++;
                 if (index == 30) index = 1;
             }
--- SportsAgencyTycoon/ViewStandingsForm.Designer.cs
+++ SportsAgencyTycoon/ViewStandingsForm.Designer.cs
@@ -53,90 +53,100 @@
             // lblWestConference
             // 
             this.lblWestConference.AutoSize = true;
+            this.lblWestConference.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWestConference.Location = new System.Drawing.Point(13, 40);
             this.lblWestConference.Name = "lblWestConference";
-            this.lblWestConference.Size = new System.Drawing.Size(35, 13);
+            this.lblWestConference.Size = new System.Drawing.Size(35, 15);
             this.lblWestConference.TabIndex = 3;
             this.lblWestConference.Text = "label1";
             // 
             // lblEastConference
             // 
             this.lblEastConference.AutoSize = true;
+            this.lblEastConference.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEastConference.Location = new System.Drawing.Point(217, 40);
             this.lblEastConference.Name = "lblEastConference";
-            this.lblEastConference.Size = new System.Drawing.Size(35, 13);
+            this.lblEastConference.Size = new System.Drawing.Size(35, 15);
             this.lblEastConference.TabIndex = 4;
             this.lblEastConference.Text = "label2";
             // 
             // lblWestDivision1
             // 
             this.lblWestDivision1.AutoSize = true;
+            this.lblWestDivision1.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWestDivision1.Location = new System.Drawing.Point(422, 40);
             this.lblWestDivision1.Name = "lblWestDivision1";
-            this.lblWestDivision1.Size = new System.Drawing.Size(35, 13);
+            this.lblWestDivision1.Size = new System.Drawing.Size(35, 15);
             this.lblWestDivision1.TabIndex = 5;
             this.lblWestDivision1.Text = "label3";
             // 
             // lblWestDivision2
             // 
             this.lblWestDivision2.AutoSize = true;
+            this.lblWestDivision2.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWestDivision2.Location = new System.Drawing.Point(422, 160);
             this.lblWestDivision2.Name = "lblWestDivision2";
-            this.lblWestDivision2.Size = new System.Drawing.Size(35, 13);
+            this.lblWestDivision2.Size = new System.Drawing.Size(35, 15);
             this.lblWestDivision2.TabIndex = 6;
             this.lblWestDivision2.Text = "label4";
             // 
             // lblWestDivision3
             // 
             this.lblWestDivision3.AutoSize = true;
+            this.lblWestDivision3.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWestDivision3.Location = new System.Drawing.Point(422, 280);
             this.lblWestDivision3.Name = "lblWestDivision3";
-            this.lblWestDivision3.Size = new System.Drawing.Size(35, 13);
+            this.lblWestDivision3.Size = new System.Drawing.Size(35, 15);
             this.lblWestDivision3.TabIndex = 7;
             this.lblWestDivision3.Text = "label5";
             // 
             // lblWestDivision4
             // 
             this.lblWestDivision4.AutoSize = true;
+            this.lblWestDivision4.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWestDivision4.Location = new System.Drawing.Point(422, 400);
             this.lblWestDivision4.Name = "lblWestDivision4";
-            this.lblWestDivision4.Size = new System.Drawing.Size(35, 13);
+            this.lblWestDivision4.Size = new System.Drawing.Size(35, 15);
             this.lblWestDivision4.TabIndex = 8;
             this.lblWestDivision4.Text = "label6";
             // 
             // lblEastDivision1
             // 
             this.lblEastDivision1.AutoSize = true;
+            this.lblEastDivision1.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEastDivision1.Location = new System.Drawing.Point(617, 40);
             this.lblEastDivision1.Name = "lblEastDivision1";
-            this.lblEastDivision1.Size = new System.Drawing.Size(35, 13);
+            this.lblEastDivision1.Size = new System.Drawing.Size(35, 15);
             this.lblEastDivision1.TabIndex = 9;
             this.lblEastDivision1.Text = "label7";
             // 
             // lblEastDivision2
             // 
             this.lblEastDivision2.AutoSize = true;
+            this.lblEastDivision2.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEastDivision2.Location = new System.Drawing.Point(617, 160);
             this.lblEastDivision2.Name = "lblEastDivision2";
-            this.lblEastDivision2.Size = new System.Drawing.Size(35, 13);
+            this.lblEastDivision2.Size = new System.Drawing.Size(35, 15);
             this.lblEastDivision2.TabIndex = 10;
             this.lblEastDivision2.Text = "label8";
             // 
             // lblEastDivision3
             // 
             this.lblEastDivision3.AutoSize = true;
+            this.lblEastDivision3.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEastDivision3.Location = new System.Drawing.Point(617, 280);
             this.lblEastDivision3.Name = "lblEastDivision3";
-            this.lblEastDivision3.Size = new System.Drawing.Size(35, 13);
+            this.lblEastDivision3.Size = new System.Drawing.Size(35, 15);
             this.lblEastDivision3.TabIndex = 11;
             this.lblEastDivision3.Text = "label9";
             // 
             // lblEastDivision4
             // 
             this.lblEastDivision4.AutoSize = true;
+            this.lblEastDivision4.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEastDivision4.Location = new System.Drawing.Point(617, 400);
             this.lblEastDivision4.Name = "lblEastDivision4";
-            this.lblEastDivision4.Size = new System.Drawing.Size(41, 13);
+            this.lblEastDivision4.Size = new System.Drawing.Size(41, 15);
             this.lblEastDivision4.TabIndex = 12;
             this.lblEastDivision4.Text = "label10";
             // 
