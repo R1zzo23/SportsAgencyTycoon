ffa9433bdbf05b41236886b1f2f57db2c3ae6230
Adam Rizzo <arizzo.dev@gmail.com>
2019-09-12 12:43:02 -0400

    added Skill enum; all basketballPlayers now have a Strength added

--- SportsAgencyTycoon/BasketballPlayer.cs
+++ SportsAgencyTycoon/BasketballPlayer.cs
@@ -19,6 +19,7 @@ namespace SportsAgencyTycoon
         public double Assists = 0.0;
         public double Steals = 0.0;
         public double Blocks = 0.0;
+        public Skill Strength;
 
         public BasketballPlayer(Random rnd, int id, Sports sport, int age, Position position)
             : base(rnd, id, sport, age)
@@ -29,6 +30,52 @@ namespace SportsAgencyTycoon
             TitlesWon = 0;
             AllStarTeams = 0;
             AllNBATeams = 0;
+            DetermineStrength(Position, rnd);
+        }
+
+        public void DetermineStrength(Position p, Random rnd)
+        {
+            int strengthNumber = rnd.Next(1, 101);
+            if (p == Position.PG)
+            {
+                if (strengthNumber <= 3) Strength = Skill.Blocking;
+                else if (strengthNumber <= 15) Strength = Skill.Rebounding;
+                else if (strengthNumber <= 40) Strength = Skill.Stealing;
+                else if (strengthNumber <= 70) Strength = Skill.Passing;
+                else Strength = Skill.Scoring;
+            }
+            else if (p == Position.SG)
+            {
+                if (strengthNumber <= 5) Strength = Skill.Blocking;
+                else if (strengthNumber <= 18) Strength = Skill.Rebounding;
+                else if (strengthNumber <= 42) Strength = Skill.Stealing;
+                else if (strengthNumber <= 67) Strength = Skill.Passing;
+                else Strength = Skill.Scoring;
+            }
+            else if (p == Position.SF)
+            {
+                if (strengthNumber <= 8) Strength = Skill.Blocking;
+                else if (strengthNumber <= 22) Strength = Skill.Rebounding;
+                else if (strengthNumber <= 45) Strength = Skill.Stealing;
+                else if (strengthNumber <= 65) Strength = Skill.Passing;
+                else Strength = Skill.Scoring;
+            }
+            else if (p == Position.PF)
+            {
+                if (strengthNumber <= 20) Strength = Skill.Blocking;
+                else if (strengthNumber <= 45) Strength = Skill.Rebounding;
+                else if (strengthNumber <= 52) Strength = Skill.Stealing;
+                else if (strengthNumber <= 64) Strength = Skill.Passing;
+                else Strength = Skill.Scoring;
+            }
+            else if (p == Position.C)
+            {
+                if (strengthNumber <= 30) Strength = Skill.Blocking;
+                else if (strengthNumber <= 65) Strength = Skill.Rebounding;
+                else if (strengthNumber <= 72) Strength = Skill.Stealing;
+                else if (strengthNumber <= 75) Strength = Skill.Passing;
+                else Strength = Skill.Scoring;
+            }
         }
     }
 }
